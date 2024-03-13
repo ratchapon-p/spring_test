678300f9fdedb997737fd5d32b7c2fa7b9f5fd3b
ratchapon-p <133362486+ratchapon-p@users.noreply.github.com>
2024-03-13 14:34:54 +0700

    my first commit

---
+++ springfromvscode/src/main/java/com/ratchapon/springfromvscode/SpringfromvscodeApplication.java
@@ -0,0 +1,13 @@
+package com.ratchapon.springfromvscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfromvscodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfromvscodeApplication.class, args);
+	}
+
+}
